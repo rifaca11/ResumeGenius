8ee59aac076782be438a87679619d285f3ff9086
Elkafhi-Youssef <elkafhiyoussef@gmail.com>
2022-12-18 17:10:33 +0100

    start project

---
+++ src/main/java/com/resumegenius/ResumeGenius/ResumeGeniusApplication.java
@@ -0,0 +1,13 @@
+package com.resumegenius.ResumeGenius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeGeniusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeGeniusApplication.class, args);
+	}
+
+}
